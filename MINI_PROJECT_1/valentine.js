ecf6653c891d21f604af0d352897eb66fb7e253e
Kuldeep Gogoi <125810012+kuldeep10011@users.noreply.github.com>
2024-02-08 19:50:42 +0530

    Add files via upload

---
+++ MINI_PROJECT_1/valentine.js
@@ -0,0 +1,16 @@
+function moveButton() {
+    var x =
+      Math.random() *
+      (window.innerWidth -
+        document.getElementById("noButton").offsetWidth);
+    var y =
+      Math.random() *
+      (window.innerHeight -
+        document.getElementById("noButton").offsetHeight);
+    document.getElementById("noButton").style.left = `${x}px`;
+    document.getElementById("noButton").style.top = `${y}px`;
+      }
+
+      function nextPage() {
+        window.location.href = "yes.html";
+      }
